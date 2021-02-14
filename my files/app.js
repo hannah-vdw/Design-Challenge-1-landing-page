c7db3145c4e3971da561a67ccd0c7f220af7f716
Hannah <hannah.v.d.wijngaard@gmail.com>
2021-02-14 19:54:04 +0000

    final updates to design, fix js button functionality

--- my files/app.js
+++ my files/app.js
@@ -1,25 +1,22 @@
-console.log("HIYA")
 
 // const btns = document.querySelectorAll('.p-button');
-const psheets = document.querySelectorAll('.pricesheet');
+const psheets = document.querySelectorAll('.sheet');
 
 
-function addStyles() {
-    this.classList.add('pselected')
-    this.children[1].classList.add('btnselected')
-}
+psheets.forEach(psheet => {
 
-function removeStyles() {
-    this.classList.remove('pselected')
-    this.children[1].classList.remove('btnselected')
-}
+    psheet.addEventListener('click', function() {
 
+        psheets.forEach(psht => {
+            psht.classList.remove('pselected')
+            psht.children[1].classList.remove('btnselected')
 
-for (let psheet of psheets) {
-    psheet.addEventListener('mouseover', addStyles);
-    psheet.addEventListener('mouseout', removeStyles);
-    psheet.addEventListener('click', addStyles);
-}
+            this.classList.add('pselected')
+            this.children[1].classList.add('btnselected')   
+        })
+    })
+})
 
 
-// when click on .pricesheet add class selected and remove from all other items
+
+
